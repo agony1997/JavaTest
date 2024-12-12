a0a32c3a8d3c9a87023e01e01b13ed96f0665fac
agony1997 <a03046578@gmail.com>
2024-12-12 10:28:54 +0800

    update : if else expression

--- src/main/kotlin/org/kotlin/school/Student.kt
+++ src/main/kotlin/org/kotlin/school/Student.kt
@@ -3,18 +3,25 @@ package org.kotlin.school
 import java.util.*
 
 fun main() {
-    Student(null,60,80).checkName()
 
-/*    Student("wang",50)
+
+    println(Student(null, 60, 80).getTop())
+
+}
+
+private fun test() {
+
+
+    Student("wang", 50)
     Student()
 
     BigStudent()
-    BigStudent("big",20,15)*/
+    BigStudent("big", 20, 15)
 }
 
-class Student(var name : String?, var mathScore: Int,var artScore: Int) {
+class Student(var name: String?, var mathScore: Int, var artScore: Int) {
 
-    constructor() : this("",0,0) {
+    constructor() : this("", 0, 0) {
         val scanner = Scanner(System.`in`)
         print("Enter name")
         name = scanner.nextLine()
@@ -30,7 +37,23 @@ class Student(var name : String?, var mathScore: Int,var artScore: Int) {
         printScore()
     }
 
-    fun checkName(){
+    fun getTop() : Int{
+        var top : Int
+        top = if (mathScore > artScore) {
+            mathScore
+        } else if (artScore > mathScore) {
+            artScore
+        } else {
+            println("have multiple top score")
+            mathScore
+        }
+        return top
+    }
+
+//    An expression is any valid unit of code that resolves to a value.
+//    > 任何會回傳值的程式碼片段都算是表達式
+
+    fun checkName() {
         println("--- ${name?.get(2)}")
         try {
             println(name!!.length)
@@ -41,15 +64,15 @@ class Student(var name : String?, var mathScore: Int,var artScore: Int) {
         println("over")
     }
 
-    fun printScore(){
+    fun printScore() {
         println("Name = $name, MathScore = $mathScore, ArtScore = $artScore")
     }
 }
 
 class BigStudent {
-    var name : String
-    var mathScore : Int
-    var artScore : Int
+    var name: String
+    var mathScore: Int
+    var artScore: Int
 
     constructor(name: String, mathScore: Int, artScore: Int) {
         this.name = name
@@ -58,12 +81,12 @@ class BigStudent {
         printScore()
     }
 
-    constructor() : this("",0,0) {
+    constructor() : this("", 0, 0) {
         println("無參建構")
         printScore()
     }
 
-    fun printScore(){
+    fun printScore() {
         println("Name = $name, MathScore = $mathScore, ArtScore = $artScore")
     }
 }
